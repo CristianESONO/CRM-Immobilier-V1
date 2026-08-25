f8639bedbac1f8e381c617f04628ac6fe7be704f
Cristian ESONO <ruslan-esono.maye@ism.edu.sn>
2026-08-25 11:15:28 +0000

    Set brand name to CRM Immobilier in Filament provider and .env

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('CRM Immobilier')
             ->colors([
                 'primary' => Color::Amber,
             ])
